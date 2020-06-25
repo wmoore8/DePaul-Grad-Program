634e66f8c828e9979b1e5e255eca5fd55b5d04ce
wmoore8 <38674278+wmoore8@users.noreply.github.com>
2020-06-25 15:37:54 -0500

    Update AppDelegate.swift

--- CSC471 - Mobile iOS Development/Final Project/MooreW_final/MooreW_final/AppDelegate.swift
+++ CSC471 - Mobile iOS Development/Final Project/MooreW_final/MooreW_final/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyD1lnK6tZvMpf9HD3Dhi2h3U1bW8--E6vs")
+        GMSServices.provideAPIKey("TODO HIDE THIS API KEY")
         return true
     }
 
